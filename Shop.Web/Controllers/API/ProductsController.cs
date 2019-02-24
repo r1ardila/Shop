9d91d366fe15dad93f6b83d038737619e6b139a0
r1ardila <ardiladm@hotamil.com>
2019-02-24 15:00:37 -0500

    API controller

---
+++ Shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Shop.Web.Data;
+
+[Route("api/[Controller]")]
+public class ProductsController : Controller
+{
+    private readonly IProductRepository productRepository;
+
+    public ProductsController(IProductRepository productRepository)
+    {
+        this.productRepository = productRepository;
+    }
+
+    [HttpGet]
+    public IActionResult GetProducts()
+    {
+        return this.Ok(this.productRepository.GetAll());
+    }
+}
