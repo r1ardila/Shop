3ac6b865a556209898f6ddcd098e8bc4ea66106e
r1ardila <ardiladm@hotamil.com>
2019-03-10 01:37:31 -0500

    video 27 terminado

--- Shop.Web/Helpers/IUserHelper.cs
+++ Shop.Web/Helpers/IUserHelper.cs
@@ -16,6 +16,10 @@
 
         Task LogoutAsync();
 
+        Task<IdentityResult> UpdateUserAsync(User user);
+
+        Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
+
 
 
     }
